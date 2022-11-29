d10edddcaf5645831c7c96794a1702cca1a80e12
odarbejr <ernesto@mybusybee.net>
2022-11-29 17:05:33 +0800

    fixed link iconn

---
+++ wp-content/themes/iGov-child/course-scheduling-management-module/course-cards.php
@@ -0,0 +1,57 @@
+<div class="row" style="margin-left:40px !important;">
+   
+        <div style="width: 160px;">
+            <a class="card-block stretched-link text-decoration-none" href="/course-schedule-proposal/">
+                <div class="card p-1 card-nav card-red">
+                    <img src="/course-scheduling-management-module/icon/schedule.png" alt="" width="50">
+                    <p class="card-title mt-2 mb-0">Propose Schedule</p>
+                    
+                </div>
+            </a>
+        </div>
+        <div class="" style="width: 160px; margin-left:20px;">
+            <a class="card-block stretched-link text-decoration-none" href="/block-section/">
+                <div class="card p-1 card-nav card-blue1">
+                    <img src="/course-scheduling-management-module/icon/section.png" alt="" width="50">
+                    <p class="card-title mt-2 mb-0">Block Section</p>
+               
+                </div>
+            </a>
+        </div>
+        <div class="" style="width: 160px; margin-left:20px;">
+            <a class="card-block stretched-link text-decoration-none" href="">
+                <div class="card p-1 card-nav card-green">
+                    <img src="/course-scheduling-management-module/icon/approved.png" alt="" width="50">
+                    <p class="card-title mt-2 mb-0">Approve Schedule</p>
+                  
+                </div>
+            </a>
+        </div>
+        <div class="" style="width: 160px; margin-left:20px;">
+            <a class="card-block stretched-link text-decoration-none" href="/approve-schedule/">
+                <div class="card p-1 card-nav card-blue2">
+                    <img src="/course-scheduling-management-module/icon/plot.png" alt="" width="50">
+                    <p class="card-title mt-2 mb-0">Plot Schedule</p>
+                   
+                </div>
+            </a>
+        </div>
+        <div class="" style="width: 160px; margin-left:20px;">
+            <a class="card-block stretched-link text-decoration-none" href="">
+                <div class="card p-1 card-nav card-blue2">
+                    <img src="/course-scheduling-management-module/icon/finalize.png" alt="" width="50">
+                    <p class="card-title mt-2 mb-0">Finalize Schedule</p>
+                 
+                </div>
+            </a>
+        </div>
+        <div class="" style="width: 160px; margin-left:20px;">
+            <a class="card-block stretched-link text-decoration-none" href="">
+                <div class="card p-1 card-nav card-blue2">
+                    <img src="/course-scheduling-management-module/icon/finalize.png" alt="" width="50">
+                    <p class="card-title mt-2 mb-0">Professor List</p>
+                 
+                </div>
+            </a>
+        </div>
+</div>
