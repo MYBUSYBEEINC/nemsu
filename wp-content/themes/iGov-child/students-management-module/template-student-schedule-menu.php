40f78afe1bd3d46d839a7a130f2621561185e672
mybusybeedale <dale@mybusybee.net>
2022-12-01 15:20:02 +0800

    MODIFIED: shift_student

---
+++ wp-content/themes/iGov-child/students-management-module/template-student-schedule-menu.php
@@ -0,0 +1,38 @@
+<?php //Template Name: Student Schedule ?>
+<?php get_header(); ?>
+
+	<div class="class-wrapper-menu">
+      <div class="class-container">
+        <div class="student-menu">
+            <ul>
+                <li>
+                    <a class="single-block" href="">
+                    <i class="fas fa-calendar"></i>
+                    <h3>View Student Schedule</h3>
+                    </a>
+                </li>
+                <li>
+                    <a class="single-block" href="">
+                    <i class="fas fa-exchange-alt"></i>
+                    <h3>Shift Student</h3>
+                    </a>
+                    
+                </li>
+                <li>
+                    <a class="single-block" href="">
+                    <i class="fas fa-user-plus"></i>
+                    <h3>Add Student/ <br>Assign Student Course</h3>
+                    </a>
+                    
+                </li>
+                
+            </ul>
+        </div>
+      </div>
+    </div>
+
+
+    
+	
+
+<?php get_footer(); ?>
